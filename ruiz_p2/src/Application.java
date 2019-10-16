fd6b8700a79b9722284accb41af2c935d1ecc06a
Ricardo Ruiz <kxngvenom@gmail.com>
2019-10-15 20:34:30 -0400

    Project 3 Problem 2 Complete

--- ruiz_p2/src/Application.java
+++ ruiz_p2/src/Application.java
@@ -2,6 +2,34 @@ public class Application {
 
     public static void main(String[] args) {
 
+        SavingsAccount sav1 = new SavingsAccount();
+        SavingsAccount sav2 = new SavingsAccount();
 
+        SavingsAccount.modifyAnnualInterestRate(4.0);
+
+        sav1.setSavingsBalance(2000.00);
+        sav2.setSavingsBalance(2000.00);
+
+        for(int i = 1; i <= 12; i++){
+            System.out.println("Month " + i + "-------------- Interest Rate: " + SavingsAccount.getAnnualInterestRate());
+            System.out.printf("Saver 1: Original Balance: %.2f\n", sav1.getSavingsBalance());
+            sav1.calculateMonthlyInterest();
+            System.out.printf("         New Balance: %.2f\n\n", sav1.getSavingsBalance());
+            System.out.printf("Saver 2: Original Balance: %.2f\n", sav2.getSavingsBalance());
+            sav2.calculateMonthlyInterest();
+            System.out.printf("         New Balance: %.2f\n\n", sav2.getSavingsBalance());
+        }
+
+        SavingsAccount.modifyAnnualInterestRate(5.0);
+
+        for(int i = 1; i <= 1; i++){
+            System.out.println("Month " + i + "-------------- Interest Rate: " + SavingsAccount.getAnnualInterestRate());
+            System.out.printf("Saver 1: Original Balance: %.2f\n", sav1.getSavingsBalance());
+            sav1.calculateMonthlyInterest();
+            System.out.printf("         New Balance: %.2f\n\n", sav1.getSavingsBalance());
+            System.out.printf("Saver 2: Original Balance: %.2f\n", sav2.getSavingsBalance());
+            sav2.calculateMonthlyInterest();
+            System.out.printf("         New Balance: %.2f\n\n", sav2.getSavingsBalance());
+        }
     }
 }
--- ruiz_p2/src/SavingsAccount.java
+++ ruiz_p2/src/SavingsAccount.java
@@ -1,3 +1,31 @@
 public class SavingsAccount {
 
+    private static double annualInterestRate;
+    private double savingsBalance;
+
+    public SavingsAccount(){
+        savingsBalance = 0;
+    }
+
+    public static void modifyAnnualInterestRate(double interestRate){
+        annualInterestRate = interestRate / 100.0;
+    }
+
+    public static double getAnnualInterestRate(){
+        return annualInterestRate * 100;
+    }
+
+
+    public void setSavingsBalance(double balance){
+        savingsBalance = balance;
+    }
+
+    public double getSavingsBalance() {
+        return savingsBalance;
+    }
+
+    public void calculateMonthlyInterest(){
+        savingsBalance = ((savingsBalance * annualInterestRate) / 12.0) + savingsBalance;
+    }
+
 }
